d06da5206cbc4c8c09c9358a22a816919f1c636c
GabrielHiguti <bieltadashi2003@hotmail.com>
2023-10-06 08:39:01 -0300

    Aula1

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
+import Input from './components/input';
+import Button from './components/button';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image style={styles.image} source={require('./assets/merca2.jpg')} />
+      <Text style={styles.titulo}>Zé</Text>
+      <Text style={styles.title}>Mercado</Text>
+
+      <Input placeholder='Username'/>
+      <Input placeholder='Password' secureTextEntry/>
+
+      <Button>Entrar</Button>
+
+      <TouchableOpacity>
+        <Text>Criar Conta</Text>
+      </TouchableOpacity>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +28,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FCFBFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  title:{
+    color: '#FFBA26',
+    fontSize: 34,
+    fontWeight: 'bold',
+  },
+
+  titulo:{
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 50,
+  },
+
+  image: {
+    height: 200,
+    width: '100%',
+    position: 'absolute',
+    top: 0,
+  }
+
 });
